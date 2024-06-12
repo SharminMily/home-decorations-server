757fa419e8d955bc0ee448895fedefdb60d5b737
SharminMily <sharminmily@gmail.com>
2024-06-12 22:29:57 +0600

    update

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    
+    // 'https://home-decorations-cde79.web.app'   
+    'http://home-decorations.surge.sh'   
+ 
   ],
   credentials: true
 }));
@@ -33,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const servicesCollection = client.db("homeDecorations").collection("allServices")
 
@@ -55,9 +57,9 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
